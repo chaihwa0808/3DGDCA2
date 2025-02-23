e0420dd6e3174fca76eda686d0c467873a021ea2
chaihwa0808 <ct945717@gmail.com>
2025-02-24 07:26:50 +0800

    timer + main menu

--- 3DGD_CA2/Assets/Scripts/C#/CountDownTime.cs
+++ 3DGD_CA2/Assets/Scripts/C#/CountDownTime.cs
@@ -1,48 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-[ExecuteInEditMode()]
+public class CountdownTimer : MonoBehaviour
+{
+    [SerializeField] public float timeRemaining = 2000f;  // Set the starting time in seconds
+    [SerializeField] private TextMeshProUGUI timerText;  // The UI Text element to display the countdown
 
-public class CountDownTime : MonoBehaviour {
-	private float startTime; // Time given to complete game
-	private float timeRemaining;
-	private string timeString;
-	public GUIStyle customStyle;
-	
-	void Start() {
-		startTime = 300;
-	}
+    private bool timerRunning = false;
 
-	void Update() {
-		CountDown();
-	}
+    void Start()
+    {
+        if (timerText != null)
+        {
+            timerText.text = FormatTime(timeRemaining);  // Display the initial time
+            timerRunning = true;
+        }
+    }
 
-	void CountDown() {
-		timeRemaining = startTime - Time.timeSinceLevelLoad;
-		ShowTime();
+    void Update()
+    {
+        if (timerRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;  // Decrease the remaining time
+                timerText.text = FormatTime(timeRemaining);  // Update the UI with the new time
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerRunning = false;  // Stop the timer when it reaches zero
+                timerText.text = "Time's Up!";
+                // Optionally, you can trigger any event when the time is up here
+            }
+        }
+    }
 
-		if(timeRemaining < 0) {
-			timeRemaining = 0;
-			TimeIsUp();
-		}
-	}
+    // Start the countdown timer
+    public void StartTimer()
+    {
+        timerRunning = true;
+    }
 
-	void ShowTime() {
-		int minutes;
-		int seconds;
-
-		minutes = (int)timeRemaining / 60; // Derive minutes by dividing seconds by 60 seconds
-		seconds = (int)timeRemaining % 60; // Derive remainder after dividing by 60 seconds
-		timeString = "TIME LEFT > " + minutes.ToString() + ":" + seconds.ToString("d2");
-	}
-
-	void TimeIsUp() {
-		SceneManager.LoadScene("Lose");
-	}
-
-	void OnGUI() {
-		GUI.Label(new Rect((Screen.width - 300) * 0.5f, 662, 300, 20), timeString, customStyle);
-	}
+    // Format the time as minutes:seconds (e.g., 02:30)
+    private string FormatTime(float time)
+    {
+        int minutes = Mathf.FloorToInt(time / 60f);
+        int seconds = Mathf.FloorToInt(time % 60f);
+        return string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
 }
